cfa3b412003114233896d6ba4393365baa526820
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-02-20 00:14:57 +0100

    add test

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,5 +1,6 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { TabService } from './../services/tab.service/tab.service';
+import { AuthService } from './../services/auth.service/auth.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import {
   createComponentFactory,
@@ -8,10 +9,13 @@ import {
   Spectator,
   SpectatorHttp
 } from '@ngneat/spectator';
-import { environment } from '../../environments/environment';
-import { AuthService } from './../services/auth.service/auth.service';
-import { TabService } from './../services/tab.service/tab.service';
 import { HomeComponent } from './home.component';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import { environment } from '../../environments/environment';
 
 describe('HomeComponent', () => {
   let spectator: Spectator<HomeComponent>;
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,13 +1,22 @@
+import { AuthService } from './../services/auth.service/auth.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterTestingModule } from '@angular/router/testing';
-import { createComponentFactory, Spectator } from '@ngneat/spectator';
-import { AuthService } from '../services/auth.service/auth.service';
+import {
+  createComponentFactory,
+  createHttpFactory,
+  HttpMethod,
+  Spectator,
+  SpectatorHttp
+} from '@ngneat/spectator';
 import { LoginComponent } from './login.component';
+import { environment } from '../../environments/environment';
 
 describe('LoginComponent', () => {
   let spectator: Spectator<LoginComponent>;
+  let authService: SpectatorHttp<AuthService>;
+
   const createComponent = createComponentFactory({
     component: LoginComponent,
     imports: [
@@ -18,8 +27,12 @@ describe('LoginComponent', () => {
     ],
     providers: [AuthService]
   });
+  const createHttp = createHttpFactory(AuthService);
 
-  beforeEach(() => (spectator = createComponent()));
+  beforeEach(() => {
+    spectator = createComponent();
+    authService = createHttp();
+  });
 
   it('should create the app', () => {
     expect(spectator.component).toBeTruthy();
@@ -28,5 +41,48 @@ describe('LoginComponent', () => {
   it('Should display the title', () => {
     spectator.fixture.detectChanges();
     expect(spectator.query('h1')?.textContent).toEqual('Dash');
+  });
+
+  it('Should prevent login', () => {
+    const loginSpy = jest.spyOn(authService.service, 'login');
+    expect(spectator.component.inputUsername).toBe('');
+    expect(spectator.component.inputPassword).toBe('');
+    spectator.component.handleLogin();
+    expect(loginSpy).toBeCalledTimes(0);
+    spectator.fixture.detectChanges();
+  });
+
+  it('Should login', () => {
+    const userData = {
+      accessToken: 'accessToken',
+      id: 2,
+      username: 'admintest',
+      email: 'admin@email.com',
+      roles: ['ROLE_ADMIN'],
+      tokenType: 'Bearer'
+    };
+    const loginSpy = jest.spyOn(authService.service, 'login');
+    spectator.component.inputUsername = 'username';
+    spectator.component.inputPassword = 'password';
+    spectator.component.handleLogin();
+    const request = authService.expectOne(
+      environment.backend_url + '/auth/login',
+      HttpMethod.POST
+    );
+    request.flush(userData);
+    spectator.fixture.detectChanges();
+    expect(loginSpy).toHaveBeenCalledWith('username', 'password');
+  });
+
+  it('Should throw an error while trying to login', () => {
+    spectator.component.inputUsername = 'username';
+    spectator.component.inputPassword = 'password';
+    spectator.component.handleLogin();
+    const request = authService.expectOne(
+      environment.backend_url + '/auth/login',
+      HttpMethod.POST
+    );
+    request.flush(new Error());
+    spectator.fixture.detectChanges();
   });
 });
