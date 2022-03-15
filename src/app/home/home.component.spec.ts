9cce8a3b0dbc9029ab76c762888d669e6a249500
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-03-15 21:12:19 +0100

    home tests

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,3 +1,4 @@
+import { ITab } from './../model/Tab';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ErrorHandlerService } from './../services/error.handler.service';
 import { ConfigService } from './../services/config.service/config.service';
@@ -46,15 +47,29 @@ describe('HomeComponent', () => {
   });
 
   it('should create the app', () => {
-    expect(spectator.component).toBeTruthy();
     expect(spectator.component.tabs).toEqual([]);
     const request = tabService.expectOne(environment.backend_url + tabPath, HttpMethod.GET);
     request.flush(tabData);
-    spectator.fixture.detectChanges();
     expect(spectator.component.tabs).toEqual(tabData);
     widgetService.expectOne(
       environment.backend_url + '/widget/?tabId=' + spectator.component.activeTab,
       HttpMethod.GET
     );
+    expect(spectator.component.tabs.length).toEqual(2);
+    tabService.controller.verify();
+  });
+
+  it('Should create a tab from an empty dashboard', () => {
+    expect(spectator.component.tabs).toEqual([]);
+    const getTabsRequest = tabService.expectOne(environment.backend_url + tabPath, HttpMethod.GET);
+    getTabsRequest.flush([]);
+    spectator.component.addNewTab();
+    const createTabRequest = tabService.expectOne(
+      environment.backend_url + tabPath + 'addTab',
+      HttpMethod.POST
+    );
+    const newTabData = { id: 1, label: 'Nouvel onglet', tabOrder: 1 } as ITab;
+    createTabRequest.flush(newTabData);
+    expect(spectator.component.tabs.length).toEqual(1);
   });
 });
