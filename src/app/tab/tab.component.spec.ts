43547b688f333204653d3f53d9e3fe2a6e681c80
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-03-22 16:30:02 +0100

    tab tests

--- src/app/tab/tab.component.spec.ts
+++ src/app/tab/tab.component.spec.ts
@@ -1,18 +1,27 @@
+import { ITab } from './../model/Tab';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import {
   createComponentFactory,
   createHttpFactory,
+  HttpMethod,
   Spectator,
   SpectatorHttp
 } from '@ngneat/spectator/jest';
 import { ErrorHandlerService } from './../services/error.handler.service';
 import { TabService } from './../services/tab.service/tab.service';
 import { TabComponent } from './tab.component';
+import { environment } from '../../environments/environment';
 
 describe('TabComponent', () => {
   let spectator: Spectator<TabComponent>;
   let tabService: SpectatorHttp<TabService>;
+
+  const tabData: ITab = {
+    id: 1,
+    label: 'Flux RSS',
+    tabOrder: 1
+  };
 
   const createComponent = createComponentFactory({
     component: TabComponent,
@@ -22,9 +31,32 @@ describe('TabComponent', () => {
   });
   const createHttp = createHttpFactory(TabService);
 
-  beforeEach(() => (spectator = createComponent()));
+  beforeEach(() => {
+    spectator = createComponent();
+    tabService = createHttp();
+  });
 
-  it('Dummy test', () => {
-    expect(true).toEqual(true);
+  it('Should display and edit a tab', () => {
+    expect(spectator.component.tab).toEqual(undefined);
+    spectator.component.tab = tabData;
+    expect(spectator.component.editMode).toEqual(false);
+    spectator.component.toggleEditMode();
+    expect(spectator.component.editMode).toEqual(true);
+    const updatedTabLabel = 'Journaux';
+    spectator.component.tab.label = updatedTabLabel;
+    spectator.component.enterSaveTabName(new KeyboardEvent('keydown', { key: 'Enter' }));
+    const updatedTabData = {
+      id: 1,
+      label: updatedTabLabel,
+      tabOrder: 1
+    };
+    const request = tabService.expectOne(
+      environment.backend_url + '/tab/updateTab',
+      HttpMethod.POST
+    );
+    request.flush(updatedTabData);
+    expect(spectator.component.editMode).toEqual(false);
+    spectator.detectChanges();
+    expect(spectator.query('.tabLabel')?.textContent?.trim()).toEqual(updatedTabLabel);
   });
 });
