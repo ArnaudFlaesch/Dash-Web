bdb051456d8fa69389af07f1a5476ad033c9ea8f
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-06-02 15:59:21 +0200

    Migration

---
+++ cypress-test.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  watchForFileChanges: false,
+  defaultCommandTimeout: 8000,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  screenshotOnRunFailure: true,
+  video: false,
+  videoCompression: false,
+  env: {
+    coverage: false,
+    backend_url: 'localhost:8080'
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.ts')(on, config);
+    },
+    baseUrl: 'http://localhost:4200/',
+    specPattern: 'cypress/e2e/**/**/*.spec.ts'
+  }
+});
