6099b4bb3dc768a4ba7a33dfe9a9be829d72a39d
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-09-10 19:33:39 +0200

    Update tests

--- cypress/e2e/config/config.spec.ts
+++ cypress/e2e/config/config.spec.ts
@@ -36,7 +36,7 @@ describe('Config tests', () => {
           .intercept('DELETE', '/tab/deleteTab/*')
           .as('deleteTab')
           .get('.tab')
-          .should('have.length', 6)
+          .should('have.length', 7)
           .contains('Perso')
           .click()
           .get('.widget')
@@ -48,7 +48,7 @@ describe('Config tests', () => {
           .click()
           .wait('@deleteTab')
           .then(() => {
-            cy.get('.tab').should('have.length', 5);
+            cy.get('.tab').should('have.length', 6);
           });
       });
   });
