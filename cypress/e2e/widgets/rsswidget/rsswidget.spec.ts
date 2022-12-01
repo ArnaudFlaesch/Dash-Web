5ccda5a48ca66652262fad6c9dd2ae6333bdb350
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-12-01 15:24:42 +0100

    upgrade

--- cypress/e2e/widgets/rsswidget/rsswidget.spec.ts
+++ cypress/e2e/widgets/rsswidget/rsswidget.spec.ts
@@ -47,7 +47,9 @@ describe('RSS Widget tests', () => {
         cy.get('.rssTitle')
           .invoke('text')
           .then((text) => {
-            expect(text.trim()).equal('Le Figaro - Actualité en direct et informations en continu');
+            expect(text.trim()).equal(
+              'Le Figaro - Actualité en direct et informations en continu'
+            );
           })
           .get('.widget .rss-article')
           .should('have.length', NUMBER_OF_ARTICLES);
@@ -68,7 +70,9 @@ describe('RSS Widget tests', () => {
         cy.get('.widget .rss-article')
           .should('have.length', NUMBER_OF_ARTICLES)
           .waitUntil(() =>
-            cy.get('.widget .rss-article:nth(0)  .articleTitle').should('be.visible')
+            cy
+              .get('.widget .rss-article:nth(0)  .articleTitle')
+              .should('be.visible')
           )
           .get('.widget .rss-article:nth(0)')
           .click()
@@ -111,7 +115,10 @@ describe('RSS Widget tests', () => {
       .wait('@refreshWidget')
       .then((request: Interception) => {
         expect(request.response.statusCode).to.equal(200);
-        cy.get('.widget .rss-article').should('have.length', NUMBER_OF_ARTICLES);
+        cy.get('.widget .rss-article').should(
+          'have.length',
+          NUMBER_OF_ARTICLES
+        );
       });
   });
 
@@ -129,8 +136,9 @@ describe('RSS Widget tests', () => {
       .wait('@deleteWidgetError')
       .then((request: Interception) => {
         expect(request.response.statusCode).to.equal(500);
-        cy.get('.mat-simple-snack-bar-content')
-          .should('have.text', "Erreur lors de la suppression d'un widget.")
+        cy.shouldDisplayErrorMessage(
+          "Erreur lors de la suppression d'un widget."
+        )
           .get('.widget')
           .should('have.length', 1);
       });
