6e69aa0ff4adf905bad2e61b779f0e01768d021b
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-12-02 22:32:36 +0100

    resize

--- src/app/widgets/ecowatt-widget/ecowatt-widget.component.ts
+++ src/app/widgets/ecowatt-widget/ecowatt-widget.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-ecowatt-widget',
@@ -10,28 +16,28 @@ export class EcowattWidgetComponent {
   private iframeContainer: ElementRef | undefined;
 
   public isWidgetLoaded = true;
+  public iframeContainerHeight = 0;
+  public iframeContainerWidth = 0;
+
+  constructor(private cdRef: ChangeDetectorRef) {}
+
+  @HostListener('window:resize', ['$event'])
+  private onResize(event: Event): void {
+    this.iframeContainerHeight =
+      this.iframeContainer?.nativeElement.offsetHeight;
+    this.iframeContainerWidth = this.iframeContainer?.nativeElement.offsetWidth;
+  }
 
   public refreshWidget(): void {
-    console.log('');
-    /*
     this.cdRef.detectChanges();
-    if (this.timeline) {
+    if (this.iframeContainer) {
       const iframe =
-        this.timeline.nativeElement.getElementsByTagName('iframe')[0];
+        this.iframeContainer.nativeElement.getElementsByTagName('iframe')[0];
       if (iframe) {
         const src = iframe.src;
         iframe.src = src;
       }
     }
-    */
-  }
-
-  public getIframeHeight(): number {
-    return this.iframeContainer?.nativeElement.offsetHeight;
-  }
-
-  public getIframeWidth(): number {
-    return this.iframeContainer?.nativeElement.offsetWidth;
   }
 
   public getWidgetData(): Record<string, never> {
