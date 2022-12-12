8dcb3c61e779dc5a71faa8fb61c202c20104289d
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-12-12 17:40:38 +0100

    tests

--- src/app/widgets/twitter-widget/twitter-widget.component.spec.ts
+++ src/app/widgets/twitter-widget/twitter-widget.component.spec.ts
@@ -1,12 +1,14 @@
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import {
-  Spectator,
-  SpectatorHttp,
   createComponentFactory,
-  createHttpFactory
-} from '@ngneat/spectator';
+  createHttpFactory,
+  HttpMethod,
+  Spectator,
+  SpectatorHttp
+} from '@ngneat/spectator/jest';
 import { ErrorHandlerService } from '../../services/error.handler.service';
 
+import { IFollowedUser } from './ITwitter';
 import { TwitterWidgetComponent } from './twitter-widget.component';
 import { TwitterWidgetService } from './twitter.widget.service';
 
@@ -27,7 +29,56 @@ describe('TwitterWidgetComponent', () => {
     twitterWidgetService = createHttp();
   });
 
-  it('should create', () => {
-    expect(spectator.component).toBeTruthy();
+  const followedUserData = { id: 1, userHandle: 'Nono' } as IFollowedUser;
+
+  it('should add followed user', () => {
+    expect(spectator.component.followedUsers).toEqual([]);
+    spectator.component.searchFormControl.setValue('Nono');
+    spectator.component.addFollowedUser();
+
+    const addFollowedUserRequest = twitterWidgetService.expectOne(
+      `${environment.backend_url}/twitterWidget/addFollowedUser`,
+      HttpMethod.POST
+    );
+
+    addFollowedUserRequest.flush(followedUserData);
+
+    expect(spectator.component.followedUsers).toEqual([followedUserData]);
+  });
+
+  it('Should search followed users', () => {
+    expect(spectator.component.followedUsers).toEqual([]);
+    spectator.component.searchFormControl.setValue('test', {
+      onlySelf: false,
+      emitEvent: true
+    });
+    const searchFollowedUsersRequest = twitterWidgetService.expectOne(
+      `${environment.backend_url}/twitterWidget/followed`,
+      HttpMethod.GET
+    );
+
+    searchFollowedUsersRequest.flush([]);
+
+    expect(spectator.component.followedUsers).toEqual([]);
+  });
+
+  it('Should select a followed user and delete it', () => {
+    spectator.component.followedUsers = [followedUserData];
+
+    spectator.component.selectUserHandle(followedUserData.userHandle);
+    expect(spectator.component.selectedTwitterHandle).toEqual(
+      followedUserData.userHandle
+    );
+
+    spectator.component.deleteFollowedUser(followedUserData.id);
+
+    const removeFollowedUserRequest = twitterWidgetService.expectOne(
+      `${environment.backend_url}/twitterWidget/deleteFollowedUser?followedUserId=${followedUserData.id}`,
+      HttpMethod.DELETE
+    );
+
+    removeFollowedUserRequest.flush(null);
+
+    expect(spectator.component.followedUsers).toEqual([]);
   });
 });
