0d16d2e5921a0b517cc2784dfd2858e3a444b1ed
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2023-01-29 20:47:15 +0100

    add test

--- src/app/widgets/workout-widget/workout-session-edit/workout-session-edit.component.spec.ts
+++ src/app/widgets/workout-widget/workout-session-edit/workout-session-edit.component.spec.ts
@@ -1,52 +1,61 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import {
-  createComponentFactory,
+  createHostFactory,
   createHttpFactory,
   HttpMethod,
-  Spectator,
+  SpectatorHost,
   SpectatorHttp
 } from '@ngneat/spectator';
+
 import { environment } from '../../../../environments/environment';
 import { DateFormatPipe } from '../../../pipes/date-format.pipe';
-
 import { ErrorHandlerService } from '../../../services/error.handler.service';
 import { IWorkoutExercise, IWorkoutSession, IWorkoutType } from '../model/Workout';
 import { WorkoutWidgetService } from '../workout.widget.service';
 import { WorkoutSessionEditComponent } from './workout-session-edit.component';
 
 describe('WorkoutSessionEditComponent', () => {
-  let spectator: Spectator<WorkoutSessionEditComponent>;
+  let spectator: SpectatorHost<WorkoutSessionEditComponent>;
   let workoutWidgetService: SpectatorHttp<WorkoutWidgetService>;
 
-  const createComponent = createComponentFactory({
+  const createHost = createHostFactory({
     component: WorkoutSessionEditComponent,
     imports: [MatSnackBarModule],
     providers: [WorkoutWidgetService, ErrorHandlerService],
-    declarations: [DateFormatPipe],
-    schemas: [NO_ERRORS_SCHEMA]
+    declarations: [DateFormatPipe]
   });
+
   const createHttp = createHttpFactory(WorkoutWidgetService);
 
+  const workoutTypes = [{ id: 1, name: 'Abdos' } as IWorkoutType];
+  const newWorkoutSessionDate = new Date(2022, 8, 1, 0, 0, 0).toString();
+  const currentWorkoutSessionToEdit = {
+    id: 1,
+    workoutDate: newWorkoutSessionDate
+  } as IWorkoutSession;
+
   beforeEach(() => {
-    spectator = createComponent();
+    spectator = createHost(
+      `<app-workout-session-edit [workoutTypes]="workoutTypes" [currentWorkoutSessionToEdit]="currentWorkoutSessionToEdit"></app-workout-session-edit>`,
+      {
+        hostProps: {
+          workoutTypes: workoutTypes,
+          currentWorkoutSessionToEdit: currentWorkoutSessionToEdit
+        }
+      }
+    );
     workoutWidgetService = createHttp();
   });
 
   it('should add a rep to an exercise', () => {
-    const workoutTypes = [{ id: 1, name: 'Abdos' } as IWorkoutType];
+    const getWorkoutExercisesRequest = workoutWidgetService.expectOne(
+      environment.backend_url +
+        `/workoutWidget/workoutExercises?workoutSessionId=${currentWorkoutSessionToEdit.id}`,
+      HttpMethod.GET
+    );
+    getWorkoutExercisesRequest.flush([]);
     const workoutTypeIdToEdit = workoutTypes[0].id;
-    const newWorkoutSessionDate = new Date(2022, 8, 1, 0, 0, 0).toString();
-    const currentWorkoutSessionToEdit = {
-      id: 1,
-      workoutDate: newWorkoutSessionDate
-    } as IWorkoutSession;
-
     expect(spectator.component.workoutExercises).toEqual([]);
-
-    spectator.component.workoutTypes = workoutTypes;
-    spectator.component.currentWorkoutSessionToEdit = currentWorkoutSessionToEdit;
-    spectator.fixture.detectChanges();
 
     expect(spectator.component.getExerciceNumberOfReps(workoutTypeIdToEdit)).toEqual(0);
     spectator.component.incrementExerciceNumberOfReps(workoutTypeIdToEdit);
