4187429a1f5fd998b7b531f1d9d7d6833de2b31d
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2023-04-02 16:55:50 +0200

    add test

--- src/app/notifications/notifications.component.spec.ts
+++ src/app/notifications/notifications.component.spec.ts
@@ -29,44 +29,44 @@ describe('NotificationsComponent', () => {
   });
   const createHttp = createHttpFactory(NotificationService);
 
+  const notificationDate = new Date();
+  const notificationsData = {
+    content: [
+      {
+        id: 1,
+        message: 'username: Connexion utilisateur',
+        notificationType: NotificationTypeEnum.WARN,
+        notificationDate: notificationDate.toString(),
+        isRead: false
+      },
+      {
+        id: 2,
+        message: 'username: Action user',
+        notificationType: NotificationTypeEnum.INFO,
+        notificationDate: notificationDate.toString(),
+        isRead: false
+      },
+      {
+        id: 3,
+        message: 'username: Action user',
+        notificationType: NotificationTypeEnum.INFO,
+        notificationDate: startOfYesterday(),
+        isRead: true
+      }
+    ],
+    totalPages: 1,
+    totalElements: 3,
+    last: true,
+    size: 1,
+    number: 0
+  } as Pageable<INotification>;
+
   beforeEach(() => {
     spectator = createComponent();
     notificationsService = createHttp();
   });
 
   it('should create component and get notifications', () => {
-    const notificationDate = new Date();
-    const notificationsData = {
-      content: [
-        {
-          id: 1,
-          message: 'username: Connexion utilisateur',
-          notificationType: NotificationTypeEnum.WARN,
-          notificationDate: notificationDate.toString(),
-          isRead: false
-        },
-        {
-          id: 2,
-          message: 'username: Action user',
-          notificationType: NotificationTypeEnum.INFO,
-          notificationDate: notificationDate.toString(),
-          isRead: false
-        },
-        {
-          id: 3,
-          message: 'username: Action user',
-          notificationType: NotificationTypeEnum.INFO,
-          notificationDate: startOfYesterday(),
-          isRead: true
-        }
-      ],
-      totalPages: 1,
-      totalElements: 3,
-      last: true,
-      size: 1,
-      number: 0
-    } as Pageable<INotification>;
-
     const notificationsRequest = notificationsService.expectOne(
       environment.backend_url + '/notifications/',
       HttpMethod.GET
@@ -80,5 +80,26 @@ describe('NotificationsComponent', () => {
     expect(spectator.component.notificationsToDisplay[1].notificationTypeToDisplay).toEqual('info');
     expect(spectator.component.notificationsToDisplay[1].notificationTypeToDisplay).toEqual('info');
     expect(spectator.component.unreadNotificationsForBadge).toEqual('2');
+  });
+
+  it('Should mark all notifications as read', () => {
+    const notificationsRequest = notificationsService.expectOne(
+      environment.backend_url + '/notifications/',
+      HttpMethod.GET
+    );
+    notificationsRequest.flush(notificationsData);
+
+    spectator.component.markAllNotificationsAsRead(new Event('click'));
+
+    const markNotificationAsReadRequest = notificationsService.expectOne(
+      environment.backend_url + '/notifications/markNotificationAsRead',
+      HttpMethod.PUT
+    );
+    const updatedNotificationsData = notificationsData.content.map((notif) => {
+      return { ...notif, ...{ isRead: true } };
+    });
+    markNotificationAsReadRequest.flush(updatedNotificationsData);
+
+    expect(spectator.component.unreadNotificationsForBadge).toEqual('');
   });
 });
