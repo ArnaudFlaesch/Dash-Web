2635545ddec0998fd5b66cac357b1e0df4e84f38
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2024-11-16 15:54:31 +0100

    Refactoring

--- cypress-test.config.ts
+++ cypress-test.config.ts
@@ -1,29 +1,29 @@
-import { defineConfig } from 'cypress';
-import { displayBrowserInFullSize } from './cypress/plugins';
+import { defineConfig } from "cypress";
+import { displayBrowserInFullSize } from "./cypress/plugins";
 
 export default defineConfig({
-  watchForFileChanges: false,
-  defaultCommandTimeout: 6000,
-  viewportWidth: 1920,
-  viewportHeight: 1080,
-  screenshotOnRunFailure: true,
-  video: false,
-  videoCompression: false,
-  reporter: 'cypress-multi-reporters',
-  reporterOptions: {
-    configFile: 'reporter-e2e-config.json'
-  },
-  env: {
-    backend_url: 'localhost:8080'
-  },
   e2e: {
     retries: {
       runMode: 2
     },
     setupNodeEvents(on, config) {
-      on('before:browser:launch', displayBrowserInFullSize);
+      on("before:browser:launch", displayBrowserInFullSize);
     },
-    baseUrl: 'http://localhost:4200/',
-    specPattern: 'cypress/e2e/**/**/*.spec.ts'
+    baseUrl: "http://localhost:4200/",
+    specPattern: "cypress/e2e/**/**/*.spec.ts",
+    watchForFileChanges: false,
+    defaultCommandTimeout: 6000,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    screenshotOnRunFailure: true,
+    video: false,
+    videoCompression: false,
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      configFile: "reporter-e2e-config.json"
+    },
+    env: {
+      backend_url: "localhost:8080"
+    }
   }
 });
--- cypress.config.ts
+++ cypress.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig } from 'cypress';
-import { displayBrowserInFullSize } from './cypress/plugins';
+import { defineConfig } from "cypress";
+import { displayBrowserInFullSize } from "./cypress/plugins";
 
 export default defineConfig({
-  watchForFileChanges: false,
-  defaultCommandTimeout: 4000,
-  viewportWidth: 1920,
-  viewportHeight: 1080,
-  screenshotOnRunFailure: false,
-  video: false,
-  videoCompression: false,
-  env: {
-    coverage: false,
-    backend_url: 'localhost:8080'
-  },
   e2e: {
     setupNodeEvents(on, config) {
-      on('before:browser:launch', displayBrowserInFullSize);
+      on("before:browser:launch", displayBrowserInFullSize);
     },
-    baseUrl: 'http://localhost:4200/',
-    specPattern: 'cypress/e2e/**/**/*.spec.ts'
+    baseUrl: "http://localhost:4200/",
+    specPattern: "cypress/e2e/**/**/*.spec.ts",
+    watchForFileChanges: false,
+    defaultCommandTimeout: 4000,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    screenshotOnRunFailure: false,
+    video: false,
+    videoCompression: false,
+    env: {
+      coverage: false,
+      backend_url: "localhost:8080"
+    }
   }
 });
