b294e8fa5511546efccad4162dafbd04bb25b9d0
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2025-04-05 18:40:10 +0200

    Change Sonar action

--- src/app/widgets/incident-widget/incident-widget.component.ts
+++ src/app/widgets/incident-widget/incident-widget.component.ts
@@ -20,7 +20,7 @@ import { switchMap } from "rxjs";
   selector: "dash-incident-widget",
   templateUrl: "./incident-widget.component.html",
   styleUrls: ["./incident-widget.component.scss"],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.Default,
   standalone: true,
   imports: [
     WidgetComponent,
